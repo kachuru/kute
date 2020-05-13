c6705fe3f8fbb3f062637f5da93ba187ffbf8bef
Rupert Jones <rupert.jones@inviqa.com>
2020-05-13 08:31:25 +0100

    Update composer dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     App\AppBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
