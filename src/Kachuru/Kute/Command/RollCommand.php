ae9cd398e399cdc601749e05b0bfef0f3946c111
Rupert Jones <rupert.jones@inviqa.com>
2020-10-30 18:10:49 +0000

    Switch number of sides from an option to an argument

--- src/Kachuru/Kute/Command/RollCommand.php
+++ src/Kachuru/Kute/Command/RollCommand.php
@@ -3,6 +3,7 @@
 namespace Kachuru\Kute\Command;
 
 use App\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,11 +14,11 @@ class RollCommand extends Command
     {
         $this->setName('roll');
         $this->setDescription('Roll a dice.');
-        $this->addOption('sides', 'd', InputOption::VALUE_OPTIONAL, 'Number of sides the dice should have');
+        $this->addArgument('sides', InputArgument::OPTIONAL, 'Number of sides the dice should have');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln(sprintf('Result: %d', mt_rand(1, ($input->getOption('sides') ?? 6))));
+        $output->writeln(sprintf('Result: %d', mt_rand(1, ($input->getArgument('sides') ?? 6))));
     }
 }
