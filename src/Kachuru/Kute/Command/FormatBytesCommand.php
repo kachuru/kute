28bf875d99b63a27e6d3a8b4d47bd821efd04ba5
Rupert Jones <rupert.jones@inviqa.com>
2021-04-23 16:38:45 +0100

    Migrate fbytes script into kute

---
+++ src/Kachuru/Kute/Command/FormatBytesCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kachuru\Kute\Command;
+
+use App\Command\Command;
+
+use Kachuru\Util\Math;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FormatBytesCommand extends Command
+{
+    private Math $math;
+
+    public function __construct(Math $math)
+    {
+        parent::__construct();
+
+        $this->math = $math;
+    }
+
+    public function configure()
+    {
+        $this->setName('format-bytes');
+        $this->setAliases(['fbytes']);
+        $this->setDescription('Format bytes into human-readable format');
+        $this->addArgument('bytes', InputArgument::REQUIRED, 'Number of bytes to format');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln($this->math->getBytes($input->getArgument('bytes')));
+    }
+}
