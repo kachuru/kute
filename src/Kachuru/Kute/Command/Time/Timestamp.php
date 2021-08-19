fbfa8f5ac98648d04c3a0d7b66292bd3e2a40758
Rupert Jones <rupert.jones@inviqa.com>
2021-08-19 10:44:50 +0100

    Port time-related commands to kute commands

---
+++ src/Kachuru/Kute/Command/Time/Timestamp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kachuru\Kute\Command\Time;
+
+use App\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Timestamp extends Command
+{
+    public function configure()
+    {
+        $this->setName('time:dt2ts');
+        $this->setAliases(['dt2ts', 'timestamp']);
+        $this->setDescription('Display the epoch timestamp of the provided datetime (or the current timestamp)');
+        $this->addArgument(
+            'datetime',
+            InputArgument::OPTIONAL,
+            'The datetime (or any other compatible format) to parse'
+        );
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln((new \DateTime($input->getArgument('datetime')))->format('U'));
+    }
+}
