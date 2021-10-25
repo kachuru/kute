fb073f64d4b78f8c7b2f8a196e6a193e2a761bd1
Rupert Jones <ru@kachuru.uk>
2021-10-25 11:54:32 +0100

    Use library for roll functionality

--- src/Kachuru/Kute/Command/RollCommand.php
+++ src/Kachuru/Kute/Command/RollCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kachuru\Kute\Command;
 
 use App\Command\Command;
+use Kachuru\Util\Dice;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -12,16 +13,30 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class RollCommand extends Command
 {
+    private Dice $dice;
+
+    public function __construct(Dice $dice)
+    {
+        $this->dice = $dice;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this->setName('roll');
         $this->setDescription('Roll a dice.');
-        $this->addArgument('sides', InputArgument::OPTIONAL, 'Number of sides the dice should have');
+        $this->addArgument('format', InputArgument::OPTIONAL, 'The format of the dice to roll, e.g.: d6, 2d4, 3d5+3');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln(sprintf('Result: %d', mt_rand(1, ($input->getArgument('sides') ?? 6))));
+        $output->writeln(
+            sprintf(
+                'Rolled %s and got %d',
+                $input->getArgument('format'),
+                $this->dice->roll($input->getArgument('format'))
+            )
+        );
 
         return 0;
     }
