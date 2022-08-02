dd0ef634caf7daaa25dcf5b2e44a286f66c1fbee
Rupert Jones <ru@kachuru.uk>
2022-08-02 10:45:16 +0100

    Add JWT decode command

---
+++ src/Kachuru/Kute/Command/Jwt/DecodeCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kachuru\Kute\Command\Jwt;
+
+use App\Command\Command;
+use Firebase\JWT\JWT;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DecodeCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('jwt:decode');
+
+        $this->addArgument('jwt', InputArgument::REQUIRED);
+
+        $this->addOption('signature', 's', InputOption::VALUE_REQUIRED);
+
+        parent::configure();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(
+            print_r(
+                JWT::decode(
+                    $input->getArgument('jwt'),
+                    $input->getOption('signature'),
+                    ['HS256', 'HS384', 'HS512']
+                ),
+                true
+            )
+        );
+    }
+}
