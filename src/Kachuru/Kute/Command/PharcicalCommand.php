aa6cb8f9bcb2ce9322ff0f7251c5d87734513e8f
Rupert Jones <ru@kachuru.uk>
2024-10-22 14:08:05 +0100

    [#14] Append versions.json file when generating phar file (#23)

--- src/Kachuru/Kute/Command/PharcicalCommand.php
+++ src/Kachuru/Kute/Command/PharcicalCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kachuru\Kute\Command;
 
 use App\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,23 +14,31 @@ class PharcicalCommand extends Command
 {
     private const PHAR_FILENAME = 'kute.phar';
     private const KUTE_ENTRYPOINT = 'bin/kute';
+    private const VERSION_FILE = 'versions.json';
 
     public function configure(): void
     {
         $this->setName('pharcical');
         $this->setDescription('Create a phar file based on this project');
+        $this->addArgument(
+            'version',
+            InputArgument::REQUIRED,
+            'Version of the tool. This should be the same as a release in Github'
+        );
         $this->addOption('algorithm', 'a', InputOption::VALUE_OPTIONAL, 'Algorithm for file hash', 'SHA256');
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
         try {
+            $newVersion = $input->getArgument('version');
+
             if (ini_get('phar.readonly')) {
                 throw new \RuntimeException(
                     sprintf(
                         'Cannot create phar file with phar.readonly set to true' . PHP_EOL
                         . 'To run this command please use:' . PHP_EOL . PHP_EOL
-                        . '    php -d phar.readonly=false bin/kute pharcical' . PHP_EOL . PHP_EOL
+                        . '    php -d phar.readonly=false bin/kute pharcical ' . $newVersion . PHP_EOL . PHP_EOL
                     )
                 );
             }
@@ -44,11 +53,18 @@ class PharcicalCommand extends Command
             $phar->compressFiles(\Phar::GZ);
             chmod(self::PHAR_FILENAME, 0770);
 
-            $output->writeln(sprintf(
-                '%s: %s',
-                hash_file($input->getOption('algorithm'), self::PHAR_FILENAME),
-                self::PHAR_FILENAME
-            ));
+            $algo = $input->getOption('algorithm');
+            $fileHash = hash_file($algo, self::PHAR_FILENAME);
+            $output->writeln(sprintf('%s: %s', $fileHash, self::PHAR_FILENAME));
+
+            if (file_exists(self::VERSION_FILE)) {
+                $versions = json_decode(file_get_contents(self::VERSION_FILE), true);
+            }
+            $versions[$newVersion] = [
+                'version' => $newVersion,
+                $algo => $fileHash
+            ];
+            file_put_contents(self::VERSION_FILE, json_encode($versions, JSON_PRETTY_PRINT));
         } catch (\Exception $e) {
             echo $e->getMessage();
         }
