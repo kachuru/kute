2c7237bd3a72689de7efd633236d3010739ab270
Rupert Jones <ru@kachuru.uk>
2026-03-31 13:05:44 +0100

    [#58] Add the tickets command, and rearrange utility commands (#59)

--- src/Kachuru/Kute/Command/Utility/RollCommand.php
+++ src/Kachuru/Kute/Command/Utility/RollCommand.php
@@ -2,13 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Kachuru\Kute\Command;
+namespace Kachuru\Kute\Command\Utility;
 
 use App\Command\Command;
 use Kachuru\Util\Dice;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class RollCommand extends Command
@@ -23,7 +22,7 @@ class RollCommand extends Command
 
     protected function configure(): void
     {
-        $this->setName('roll');
+        $this->setName('utility:roll');
         $this->setDescription('Roll a dice.');
         $this->addArgument('format', InputArgument::OPTIONAL, 'The format of the dice to roll, e.g.: d6, 2d4, 3d5+3');
     }
---
+++ src/Kachuru/Kute/Command/Utility/TicketsCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kachuru\Kute\Command\Utility;
+
+use App\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TicketsCommand extends Command
+{
+    private const TICKET_FORMAT_PREG = '/^[a-f0-9]+ (\[)?(?P<ticknum>[A-Z0-9]+\-[0-9]+)(\])?[\:\- ](?P<detail>.*)$/';
+
+    public function configure(): void
+    {
+        $this
+            ->setName('utility:tickets')
+            ->setAliases(['tickets'])
+            ->setDescription('Pipe in the output of a git log command to show a list of JIRA ticket numbers')
+            ->addOption('separator', 's', InputOption::VALUE_OPTIONAL, 'Provide the list of tickets separated by this', PHP_EOL)
+            ->addOption('detailed', 'd', InputOption::VALUE_NONE, 'Show a detailed list of commits');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $separator = $input->getOption('separator');
+        $detailed = $input->getOption('detailed');
+
+        $snips = [];
+        while (false !== ($line = fgets(STDIN))) {
+            if (preg_match(self::TICKET_FORMAT_PREG, $line, $match)) {
+                $snips[] = trim($match['ticknum']) . ($detailed ? ': ' . trim($match['detail']) : '');
+            }
+        }
+
+        natsort($snips);
+        if (!$detailed) {
+            $snips = array_unique($snips);
+        }
+
+        $output->writeln(implode($separator, $snips));
+
+        return self::SUCCESS;
+    }
+}
